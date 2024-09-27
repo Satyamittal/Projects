acb2b58cc5f23517a8d7569b04e1350d192f3a90
Satyam Mittal <mittalcodes@gmail.com>
2024-09-27 17:35:07 +0530

    Router library installed , && signIn , signUp pages are connected

--- BookNest/frontened/src/App.js
+++ BookNest/frontened/src/App.js
@@ -4,15 +4,24 @@ import { BookCard } from './components_pages/c-5-book_card/bookCard.jsx';
 // import { Comp } from './components_pages/c-1-comp_name/comp_name.js'
 import { Notavail } from './components_pages/c-6-not_avail/notAvail.jsx';
 
+// For Routing
+import { createBrowserRouter, RouterProvider,createRoutesFromElements, Route } from 'react-router-dom';
+
 
 function App() {
+
+  // 1. Defining & create router 
+  const router = createBrowserRouter([
+    {path:'/',element: <SignIn></SignIn>},
+    {path:'/signUp',element: <SignUp></SignUp>},
+    {path:'/notAvailable',element: <Notavail></Notavail>}
+  ]);
+
+
   return (
     <>
-      {/* <SignIn/> */}
-      {/* <SignUp/> */}
-
-      {/* <BookCard/> */}
-      <Notavail/>
+    {/* 2. Provide router : This should be the only thing here..*/}
+      <RouterProvider router={router}/>
     </>
   )
 }
--- BookNest/frontened/src/components_pages/c-3-Sign_In/signIn.jsx
+++ BookNest/frontened/src/components_pages/c-3-Sign_In/signIn.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import signIn from './signIn.module.css'
+import {Link} from 'react-router-dom'
 
 export function SignIn() {
   return (
@@ -10,7 +11,7 @@ export function SignIn() {
               <input placeholder='Email'className={signIn.input}></input> 
               <input placeholder='Password' className={signIn.input}></input> 
               <button className={signIn.button}>Submit</button>
-              <h3 className={signIn.h3}>New User ? <a href="/signUp">Sign Up</a></h3>
+              <h3 className={signIn.h3}>New User ? <Link to="/signUp">Sign Up</Link></h3>
            </form>
       </div>
     </>
