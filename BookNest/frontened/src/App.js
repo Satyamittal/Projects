07730916aec233e3993491685109b981e887d443
Satyam Mittal <mittalcodes@gmail.com>
2024-09-27 16:42:55 +0530

    Not available page is maded

--- BookNest/frontened/src/App.js
+++ BookNest/frontened/src/App.js
@@ -2,14 +2,17 @@ import { SignIn } from './components_pages/c-3-Sign_In/signIn.jsx';
 import { SignUp } from './components_pages/c-4-Sign_Up/signUp.jsx';
 import { BookCard } from './components_pages/c-5-book_card/bookCard.jsx';
 // import { Comp } from './components_pages/c-1-comp_name/comp_name.js'
+import { Notavail } from './components_pages/c-6-not_avail/notAvail.jsx';
+
 
 function App() {
   return (
     <>
       {/* <SignIn/> */}
-      <SignUp/>
+      {/* <SignUp/> */}
 
       {/* <BookCard/> */}
+      <Notavail/>
     </>
   )
 }
---
+++ BookNest/frontened/src/components_pages/c-6-not_avail/notAvail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import notAvail from './notAvail.module.css'
+
+export function Notavail() {
+  return (
+    <div className={notAvail.flexBox}>
+            <div className={notAvail.container}></div>
+    </div>
+  )
+}
+
