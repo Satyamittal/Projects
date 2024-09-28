65b7aed1b0647aeaa972a8f9e516b54b36734822
Satyam Mittal <mittalcodes@gmail.com>
2024-09-28 10:31:48 +0530

    Buyer page is maded

--- BookNest/frontened/src/components_pages/p-3-buyer_page/buyer.page.jsx
+++ BookNest/frontened/src/components_pages/p-3-buyer_page/buyer.page.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import buyer from './buyer.module.css'
+
 import { Navbar } from '../c-8-Navbar/navbar'
 import { BookPage } from '../c-7-book_page/bookPage'
+
 
 export function PageThree() {
   return (
     <div>
       <Navbar/>
-      <div>
+      <div className={buyer.container}>
             <BookPage/>
       </div>
     </div>
